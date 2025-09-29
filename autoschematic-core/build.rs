19431352d9e89614b075239854ce350d64a854e2
Peter Sherman <pfnsec@gmail.com>
2025-09-29 01:35:23 -0700

    fix: correctly use OUT_DIR to build grpc

--- autoschematic-core/build.rs
+++ autoschematic-core/build.rs
@@ -6,7 +6,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_prost_build::configure()
         .build_server(true)
         .build_client(true)
-        .out_dir("src/grpc_generated")
         .compile_protos(proto_files, &["proto"])?;
 
     println!("cargo:rerun-if-changed=proto/connector.proto");
